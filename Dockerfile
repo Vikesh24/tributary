466b5e2b4273f0d7b43c0dc2348095ea3c497199
Vikesh S <vikesh024@gmail.com>
2024-10-29 00:06:11 +0530

    feat: add function to get engine data

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM python:3.11
 
+WORKDIR /app
+
 COPY ./requirements.txt .
 
 RUN pip install -r requirements.txt
 
-COPY ./entrypoint.py .
+COPY . /app
 
-CMD exec gunicorn entrypoint:app
+EXPOSE 8000
+
+CMD ["flask", "run", "--host=0.0.0.0", "--port=8000"]
--- entrypoint.py
+++ entrypoint.py
@@ -3,7 +3,7 @@ from crypt import methods
 
 from flask import Flask, request
 import json
-import redis as redis
+from redis import Redis
 from loguru import logger
 
 
@@ -12,11 +12,11 @@ DATA_KEY = "engine_temperature"
 
 # create a Flask server, and allow us to interact with it using the app variable
 app = Flask(__name__)
+database = Redis(host="redis", port=6379, db=0, decode_responses=True)
 
-
-@app.route('/', methods=['POST'])
-def entry():
-    return "Hello World"
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 # define an endpoint which accepts POST requests, and is reachable from the /record endpoint
 @app.route('/record', methods=['POST'])
@@ -27,7 +27,7 @@ def record_engine_temperature():
     engine_temperature = payload.get("engine_temperature")
     logger.info(f"engine temperature to record is: {engine_temperature}")
 
-    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+
     database.lpush(DATA_KEY, engine_temperature)
     logger.info(f"stashed engine temperature in redis: {engine_temperature}")
 
@@ -38,3 +38,21 @@ def record_engine_temperature():
 
     logger.info(f"record request successful")
     return {"success": True}, 200
+
+@app.route('/record', methods=['GET'])
+def get_engine_data():
+    engine_temperatures = [float(temperature) for temperature in database.lrange(DATA_KEY, 0, -1)]
+    logger.info(f"engine temperature list contains these values: {engine_temperatures}")
+
+    logger.info(f"Getting current engine temperature value")
+    current_engine_temperature = engine_temperatures[0]
+    logger.info(f"Current engine temperature is {current_engine_temperature}")
+
+    logger.info(f"Getting the averager temperature of the engine")
+    average_engine_temperature = sum(engine_temperatures) / len(engine_temperatures)
+    logger.info(f"Average temperature of the engine is {average_engine_temperature}")
+    return {
+        "success": True,
+        "current engine temperature": current_engine_temperature,
+        "average engine temperature": round(average_engine_temperature,2)
+    }, 200
